89da31467d2d268f4bff10217d3bd6ca1d48d75a
Anatoliy <morfik.russia@gmail.com>
2022-07-19 21:22:36 +0800

    ДЗ 3

--- ДЗ/Program.cs
+++ ДЗ/Program.cs
@@ -102,7 +102,107 @@ void Zadacha6() //Напишите программу, которая прини
     }
 }
 
-Zadacha5();
+void Zadacha7() //Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+{
+    Console.WriteLine("Введите любое пятизначное число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int number1 = number / 10000;
+    int number2 = (number / 1000) % 10;
+    int number3 = (number / 10) % 10;
+    int number4 = number % 10;
+    int number5 = (number / 100) % 10; // 3-я "средняя" цифра
+    if (number > 99999 || number < 10000) 
+    {
+        Console.WriteLine("Неверный ввод");
+    }
+    else if (number1 == number4 && number2 == number3)
+    {
+        Console.WriteLine($"Число является палиндромом {number} = {number}");
+    }
+    else
+    {
+        Console.WriteLine("Введеное число не является палиндромом: ");
+        Console.Write($"число {number} != {number4}{number3}{number5}{number2}{number1}");
+    }
+}
 
+void Zadacha8() //Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+{
+    Console.WriteLine("Введите x1");
+    int x1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите x2");
+    int x2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите x3");
+    int x3 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите y1");
+    int y1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите y2");
+    int y2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите y3");
+    int y3 = Convert.ToInt32(Console.ReadLine());
+    double result = Math.Sqrt(Math.Pow(x1 - y1, 2) + Math.Pow(x2 - y2, 2) + Math.Pow(x3 - y3, 2));
+    Console.WriteLine(result);
+}
 
+void Zadacha9() //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+{
+    Console.WriteLine("Введите число");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int start = 1;
+    while (start <= a)
+    {
+        Console.WriteLine(Math.Pow(start, 3));
+        start++;
+    }
+}
+
+void Zadacha10() //Дано трёхзначное число N. Определить кратна ли трём сумма всех его цифр.
+{
+    Console.WriteLine("Введите трехзначное число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int number1 = number / 100;
+    int number2 = (number / 10) % 10;
+    int number3 = number % 10;
+    int sum = number1 + number2 + number3;
+    if (sum % 3 == 0)
+    {
+        Console.WriteLine($"Сумма чисел равна {sum} и делится на 3");
+    }
+    else
+    {
+        Console.WriteLine($"Сумма чисел не равна {sum} и делится на 3");
+    }
+}
+
+void Zadacha11() //Дано трёхзначное число N. Определить, есть ли среди его цифр 4 или 7.
+{
+    Console.WriteLine("Введите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int number1 = number / 100;
+    int number2 = (number / 10) % 10;
+    int number3 = number % 10;
+    if ((number1 == 4 || number2 == 4 || number3 == 4) && (number1 == 7 || number2 == 7 || number3 == 7))
+    {
+        Console.WriteLine($"в числе {number} есть цифра 4 и цифра 7");
+    }
+    else if (number1 == 4 || number2 == 4 || number3 == 4) Console.WriteLine($"в числе {number} есть только цифра 4");
+    else if (number1 == 7 || number3 == 7 || number3 == 7) Console.WriteLine($"в числе {number} есть только цифра 7");
+    else
+    {
+        Console.WriteLine($"в числе {number} нет цифры 4 и цифры 7");
+    }
+}
+
+void Zadacha12() //Дан массив длиной 10 элементов. Заполнить его последовательно числами от 1 до 10.
+{
+    int[] array = new int[10];
+    int i = 0;
+    while (i < 10)
+    {
+        array[i] = i + 1;
+        i++;
+    }
+    Console.WriteLine("[{0}]", string.Join(", ", array)); 
+}
+Zadacha12();
 // Задачи "Заvoidены")))
